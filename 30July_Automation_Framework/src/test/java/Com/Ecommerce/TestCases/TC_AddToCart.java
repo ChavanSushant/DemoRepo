bd17705b18b3501a9531212612eab67a58399cd7
ChavanSushant <163375333+ChavanSushant@users.noreply.github.com>
2024-09-22 12:50:05 +0530

    Update TC_AddToCart.java
    Commit2

--- 30July_Automation_Framework/src/test/java/Com/Ecommerce/TestCases/TC_AddToCart.java
+++ 30July_Automation_Framework/src/test/java/Com/Ecommerce/TestCases/TC_AddToCart.java
@@ -52,5 +52,46 @@ public class TC_AddToCart extends Ecommerce_BaseClass {
         logger.info("Clciked on close Tab");
 
     }
+        @Test
+    public void AddProduct1() throws InterruptedException, IOException {
+        logger.info("Open URL");
+        TC_Ecommerce_Login_Page TEL = new TC_Ecommerce_Login_Page();
+        Thread.sleep(3000);
+        TEL.SetUsername(username);
+        System.out.println("Hello");
+        logger.info("Enter user mailL");
+        Thread.sleep(3000);
+        TEL.SetPassword(password);
+        logger.info("Enter User password");
+        Thread.sleep(3000);
+        TEL.ClickButton();
+        logger.info("Clicked on longin button");
+        Thread.sleep(3000);
+
+        TC_Ecommerce_AddToCart TEA = new TC_Ecommerce_AddToCart();
+
+        Actions Act = new Actions(driver);
+
+        WebElement Computer = driver.findElement(By.xpath("//a[contains(text(),'Computers ')]"));
+
+        Act.moveToElement(Computer).perform();
+        logger.info("Mouseover on computer");
+
+        TEA.NoteBook();
+        logger.info("Clicked on NotebookText");
+
+        TEA.SelectProduct();
+        logger.info("Product selected");
+
+        TEA.AdsToCart();
+        logger.info("Add to cart");
+
+        TEA.ProductAdded();
+        logger.info("Verified message");
+
+        TEA.Close();
+        logger.info("Clciked on close Tab");
+
+    }
 
 }
